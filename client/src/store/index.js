008262be2bef6b463a5bffa92071fd27a97f1e7c
ShadowProtocol1 <uietcseyayaativerma2125@kuk.ac.in>
2025-06-13 15:32:20 +0000

    commit

--- client/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createAuthSlice } from "./slices/auth-slice";
-import {create} from "zustand";
-import { createChatSlice } from "./slices/chat-slice";
-
-export const useAppStore = create()((...a)=>({
-    ...createAuthSlice(...a),
-    ...createChatSlice(...a),
-}))
